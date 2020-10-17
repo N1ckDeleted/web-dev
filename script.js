5cce26611ab6089377d879f71215c5fb563eaf75
Mikhail <erazer325@gmail.com>
2020-10-17 20:30:35 +0400

    First commit

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ function sumFunc (arg1, arg2, arg3) {
 }
 let result = sumFunc(1, 2, 3);
 console.log(result);
+const button = document.querySelector('#click');
+const text = document.querySelectorAll('input');
+const div = document.querySelector('#input-value');
+
+button.addEventListener('click', function (event) {
+    div.innerText = text.value;
+    console.log(event)
+})
+
+
+
+const inputListener = function (event) {
+    const input = event.target;
+    div.textContent = input.value;
+    console.log(event)
+}
+
+for (const input of text) {
+    input.addEventListener('input', inputListener);
+}
+
+// text.addEventListener('input', inputListener);
+
+
+
