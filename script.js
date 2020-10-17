1a77fad51749d32bd64cf6abb449162df2a8e378
Mikhail <erazer325@gmail.com>
2020-10-17 19:30:39 +0400

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// console.log('Step 1');
+// alert('Step 2');
+// console.log('Step 3');
+
+function sumFunc (arg1, arg2, arg3) {
+    return arg1+arg2+arg3;
+}
+let result = sumFunc(1, 2, 3);
+console.log(result);
